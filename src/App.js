bd62ec1e130b75fa5301f4b558d4bacbc0767848
adarsh <adarshurmaliya@gmail.com>
2022-03-01 21:53:33 +0530

    Posting Method implemented

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React, {useEffect} from "react";
-import useStyles from './style.js';
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import Posts from './components/Posts/posts.js'
-import Form from './components/Form/Form.js'
-import memories from "./resources/img/memories.png";
-import {useDispatch} from 'react-redux';
-import { getPosts } from './action_creator/posts';
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
-function App() {
-  const classes = useStyles();
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import { getPosts } from './action_creator/posts';
+import useStyles from './style';
+import memories from './resources/img/memories.png';
+
+const App = () => {
+  const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
-  
+  }, [currentId, dispatch]);
+
   return (
-    <Container maxidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit"  >
-        <Typography className={classes.heading} variant="h2" align="center">
-          Firewalk Prophesy
-        </Typography>
-        <img className={classes.image} src={memories} alt="Firewalk" height='60'/>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Firewalk Prophesy</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing = {3}>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
       </Grow>
     </Container>
   );
-}
+};
 
 export default App;
--- src/style.js
+++ src/style.js
@@ -10,7 +10,7 @@ export default makeStyles(() => ({
     alignItems: 'center' 
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: '#5F0587'  //'rgba(0,183,255, 1)',
   },
   image: {
     marginLeft: '15px',
