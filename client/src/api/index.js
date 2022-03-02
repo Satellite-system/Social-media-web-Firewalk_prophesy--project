33107e47c1e628c852f2de902af026ac33e0302d
adarsh <adarshurmaliya@gmail.com>
2022-03-03 03:30:19 +0530

    od

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/posts";
+const url = "https://firewalk-prophesy.herokuapp.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPosts = (newPost) => axios.post(url, newPost);
@@ -8,3 +8,4 @@ export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
 export const updatedPost = (id, updatedPost) =>
   axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
